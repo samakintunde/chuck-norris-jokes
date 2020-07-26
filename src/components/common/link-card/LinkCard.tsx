3c21aadd64fc56c03e32cba5421fb55f15751f1c
Samuel Akintunde <samakintunde37@gmail.com>
2020-07-26 21:42:14 +0100

    Build Link Card

---
+++ src/components/common/link-card/LinkCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+type LinkCardProps = {
+  to: string;
+};
+
+const LinkCard: React.FC<LinkCardProps> = (props) => {
+  const { children, to } = props;
+
+  return (
+    <Link to={to}>
+      <div className="rounded-sm border border-brand-600 min-h-10 px-8 md:px-12 py-4 md:py-6 hover:bg-brand-800">
+        <p className="text">{children}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default LinkCard;
